b6e972d3f414a4040676a254c9322844f32f377c
Aaron <aaron@regtransfers.co.uk>
2022-05-18 16:09:10 +0100

    Initial dummy project scaffolded

---
+++ HelmDeploymentDemo/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelmDeploymentDemo.Controllers;
+
+[ApiController]
+[Route("")]
+public class DefaultController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("Alive and well");
+    }
+}
